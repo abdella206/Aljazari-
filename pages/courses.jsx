5afcf1c806ea1a11ca7656f83cf3e2990c185ae9
abdella206 <f.abdella1@yahoo.com>
2020-04-23 18:43:02 -0700

    first commit

---
+++ pages/courses.jsx
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout'
+
+const Courses = () => (
+    <Layout>
+        <div>
+
+            <h1>Aljazari Courses </h1>
+            
+        </div>
+    </Layout>
+
+    
+);
+
+
+export default Courses
---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+function Home() {
+  return (
+    <Layout>
+        <div>
+            <h1>Welcome to Aljazari</h1>
+            <p>Check out our site</p>
+            
+        </div>
+    </Layout>
+  )
+}
+
+export default Home
